a0680b12a2fe65b831d9d784cbdc5d36a4f1e986
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-07-10 02:57:43 +0500

    add name & message

--- app-tg-bot.js
+++ app-tg-bot.js
@@ -4,11 +4,11 @@ const fetch = require("node-fetch");
 const token = '1878028335:AAHV6V0QsxF5iz4o5Eklgjw-WXO-WbLPEO0';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg, match) => {
+bot.onText(/\/start/, (msg) => {
 	const chatId = msg.chat.id;
-	const resp = `Hello ${chatId.firstName} , I'm smart weather bot. I can tell you current weather. Write "/weather" and your city and I send to you current weather `
+	const userName = msg.chat.first_name;
+	const resp = `Hello ${userName} , I'm smart weather bot. Write "/weather" + your city and I will send to you current weather `
 	bot.sendMessage(chatId, resp)
-	console.log(chatId)
 })
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -19,21 +19,29 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 
 bot.onText(/\/weather (.+)/, async (msg, match) => {
-        const chatId = msg.chat.id
-        const resp = match[1]
-	const currentWeather = await getWeather(resp);
-	console.log(currentWeather)
-	let response = `Temperature is ${Math.round(currentWeather.main.temp - 273, -1)} in ${currentWeather.name}`
+    try{    
+		const chatId = msg.chat.id
+        const resp = `${match[1]}`
+		console.log('###match:  ', match);
+		const currentWeather = await getWeather(resp);
+		//console.log(currentWeather)
+		const response = `Temperature is ${Math.round(currentWeather.main.temp - 273)} in ${currentWeather.name}`
         bot.sendMessage(chatId, response)
+	} catch(e) {
+		console.log(e);
+	}
 })
-
 async function getWeather(city) {
+	
 	let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ae47378a82743093b5efe8934910c74c`;
-	let response = await fetch(url);
-	let data = await response.json();
-	//console.log(data);
-	return data
+		let response = await fetch(url);
+		let data = await response.json();
+		//console.log(data);
+		return data
 }
+
+
+
 
 function  currTemp(obj) {
 	return obj.main
