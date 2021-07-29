135666650e161c65a27f4e116a6417ce49510633
Konton022 <42418105+Konton022@users.noreply.github.com>
2021-07-29 23:43:59 +0500

    add some city choose

--- app-tg-bot.js
+++ app-tg-bot.js
@@ -1,4 +1,5 @@
 'use strict'
+console.log('run...');
 // import TOKEN from './token.js'
 const TelegramBot = require('node-telegram-bot-api');
 const fetch = require("node-fetch");
@@ -6,17 +7,13 @@ const {TOKEN, yaKey, owmKey} = require('./token');
 const bot = new TelegramBot(TOKEN, { polling: true });
 const inlineWeatherKeyboard = {reply_markup: 
 								{inline_keyboard: [ 
-									[{text:'Екатеринбург', callback_data:'lat=56.837859&lon=60.598705'}],
-                                    [{text:'Кашино', callback_data:'lat=56.551287&lon=60.850492'}],
-									[{text:'Березовский', callback_data:'lat=56.909184&lon=60.822675'}],
+									[{text:'Екатеринбург', callback_data:'Ekaterinburg'}],
+                                    [{text:'Сочи', callback_data:'Sochi'}],
+									[{text:'Kemer', callback_data:'Kemer'}],
 									[{text:'...', callback_data: 'userInput'}]
 								]}
 							}
 
-
-
-//bot.on('message', (response)=> {
-//		console.log('### response: ', response)})
 
 
 bot.onText(/\/start/, (msg) => {
@@ -33,29 +30,31 @@ bot.onText(/\/weather/, (msg, match) => {
 		bot.on('callback_query', async(query)=>{
 			console.log('### query: ',query.data)
 			if (query.data !== 'userInput'){
-				const currentWeather = await getWeather(query.data)
-				//console.log(currentWeather);
+				const [lon, lat, city] = await getLonLanCoord(query.data) 
+				const currentWeather = await getWeather(lat, lon)
+				const temp = currentWeather.current.temp;
 				const {description, icon} = currentWeather.current.weather[0]
-				const response = `It's ${currentWeather.current.temp} degC and ${description}` 
+				const response = makeAnswer(city, temp, description)
 				bot.sendPhoto(query.message.chat.id, getWeatherIcon(icon),{
 				caption: response
 				}).then(()=>{
 					bot.removeAllListeners('callback_query')
 				})
-			} else
-			{
+			} else if(query.data == 'userInput'){
 				bot.sendMessage(chatId, 'Text me your city').then(()=>{
 					bot.on('message', msg=>{
 						console.log('###resp', msg.text)
 						bot.sendMessage(chatId, `${msg.text}`).then( async ()=>{
-							const coordObj = await getLonLanCoord(msg.text)
+							const [lon, lat, city] = await getLonLanCoord(msg.text)
 							bot.removeAllListeners('message')
-							console.log('cO: ',coordObj);
-							const [lon, lat] = coordObj;
 							const weatherData = await getWeather(lat, lon)
-							const {descr, ico} = weatherData.current.weather[0]
-							const response = `It's ${weatherData.current.temp} degC and ${descr}` 
-							bot.sendPhoto(query.message.chat.id, getWeatherIcon(ico),{
+							// console.log(weatherData.current.weather);
+							const temp = weatherData.current.temp;
+							const {description, icon} = weatherData.current.weather[0]
+							// console.log(description, icon);
+							// const response = `It's ${weatherData.current.temp} degC and ${description}` 
+							const response = makeAnswer(city, temp, description)
+							bot.sendPhoto(query.message.chat.id, getWeatherIcon(icon),{
 							caption: response
 							}).then(()=>{
 								bot.removeAllListeners('callback_query')
@@ -67,36 +66,10 @@ bot.onText(/\/weather/, (msg, match) => {
 			
 		})
 		
-		console.log('##### DONE ######')
+
 		
 	})
 })	
-
-
-// bot.onText(/\/weather (.+)/, async (msg, match) => {
-//     try{    
-// 		const chatId = msg.chat.id
-//         	const resp = `${match[1]}`
-// 		const currentWeather = await getWeather(resp);
-// 		const {description, icon} = currentWeather.current.weather[0];
-// 		//const htmlResp = `<strong>in ${currentWeather.name}</strong><b>It's ${(currentWeather.main.temp - 273).toFixed(2)} degC and ${description}</b>`
-// 		const response = `It's ${currentWeather.current.temp} degC and ${description} in ${currentWeather.name}`
-//         bot.sendPhoto(chatId, getWeatherIcon(icon),{caption: response})
-// 			// .then(function(data){console.log('###data: ',data)})
-// 	//bot.sendMessage(chatId, htmlResp, {parse_mode: 'HTML'})
-// 	} catch(e) {
-// 		console.log(e);
-// 	}
-// })
-// async function getWeather(city) {
-	
-// 	let url = `https://api.openweathermap.org/data/2.5/onecall?${city}&exclude=minutely&units=metric&appid=ae47378a82743093b5efe8934910c74c`;
-// 	const encoded = encodeURI(url)
-// 	let response = await fetch(encoded);
-// 	let data = await response.json();
-// 	//console.log(data);
-// 	return data
-// }
 
 function getWeatherIcon(icon){
 	return `https://openweathermap.org/img/wn/${icon}@2x.png`
@@ -105,11 +78,12 @@ function getWeatherIcon(icon){
 async function getLonLanCoord(city){			// function return [lon, lan, name, description]
 	
 	let url = `https://geocode-maps.yandex.ru/1.x?geocode=${city}&apikey=${yaKey}&format=json`;
-	let coordObj = await fetch(url);
+	const encoded = encodeURI(url)
+	let coordObj = await fetch(encoded);
 	let json = await coordObj.text();
 	json = JSON.parse(json)
-	//console.log(json);
 	let geoObject = json.response.GeoObjectCollection.featureMember[0].GeoObject;
+	console.log(geoObject);
 	let {name, description} = geoObject;
 	let points = geoObject.Point.pos;
 	console.log (points, name, description);
@@ -124,8 +98,12 @@ async function getWeather(lat, lon){ 			// function return object of weather
 	let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=daily,minutely&units=metric&appid=${owmKey}`
 	let weatherObj = await fetch(url)
 	let data = await weatherObj.json();
-	console.log(data);
+	//console.log(data);
 	return data
 	
+}
+
+function makeAnswer(city, temp, description){
+	return ` ${city} -- ${temp} -- ${description}`
 }
 
